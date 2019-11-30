8774e9ec86270c497861e00d663f7b1123c8516d
jhon-swai <jhonswai@gmail.com>
2019-11-30 10:27:27 +0300

    Added a word entity and DAO

---
+++ app/src/main/java/Word.java
@@ -0,0 +1,24 @@
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "word_table")
+public class Word {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @NonNull
+    private String word;
+
+    @ColumnInfo(name = "word")
+    private String mWord;
+
+    public Word(@NonNull String word){
+        this.mWord = word;
+    }
+
+    public String getmWord() {
+        return mWord;
+    }
+}
